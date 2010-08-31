dad864ebb2168f12aae68848b8b966d8bdfc3e3d
Terry Miller <tamiller@lanl.gov>
2010-08-31 14:23:57 -0600

    wrapping up summer work

--- src/voronoi_center.f
+++ src/voronoi_center.f
@@ -1,3 +1,5 @@
+C     voronoi_center and routines using voronoi center
+
       subroutine voronoi_center (
      1      pts11,pts12,pts13,
      2      pts21,pts22,pts23,
@@ -87,5 +89,61 @@ c
       radius = sqrt((center1-pts11)**2 +
      1              (center2-pts12)**2 +
      2              (center3-pts13)**2 )
+      return
+      end
+
+C####################
+
+      subroutine voronoi_vector_area (
+     1      x1,y1,z1,
+     1      x2,y2,z2,
+     1      x3,y3,z3,
+     5      xarea,  yarea,  zarea  )
+c
+c     This code takes 3 pts of triangle as input 
+c     and outputs the Voronoi area associated with first point 
+c     which is the sum of 2 triangles formed with 
+c     point 1, edge midpoints, voronoi center point
+c
+c     note convention has faces pointing inward
+
+      implicit none
+  
+      real*8 x1,y1,z1,x2,y2,z2,x3,y3,z3,xarea,yarea,zarea 
+      real*8 xvor,yvor,zvor,xm,ym,zm,ax1,ay1,az1,ax2,ay2,az2
+
+      xarea=0.0d+00
+      yarea=0.0d+00
+      zarea=0.0d+00
+
+      call voronoi_center_2d(x1,y1,z1,x2,y2,z2,x3,y3,z3,
+     *           xvor,yvor,zvor)
+
+C           area of right side triangle first (pt,midpt,vorpt)
+            xm=(x2+x1)*0.5d+00
+            ym=(y2+y1)*0.5d+00
+            zm=(z2+z1)*0.5d+00
+
+            ax1=  0.5d+00*((y1-ym)*(zvor-zm)-(yvor-ym)*(z1-zm))
+            ay1= -0.5d+00*((x1-xm)*(zvor-zm)-(xvor-xm)*(z1-zm))
+            az1=  0.5d+00*((x1-xm)*(yvor-ym)-(xvor-xm)*(y1-ym))
+
+C           area of left side triangle (pt,vorpt,midpt)
+            xm=(x3+x1)*0.5d+00
+            ym=(y3+y1)*0.5d+00
+            zm=(z3+z1)*0.5d+00
+
+            ax2=  0.5d+00*((y1-yvor)*(zm-zvor)-(ym-yvor)*(z1-zvor))
+            ay2= -0.5d+00*((x1-xvor)*(zm-zvor)-(xm-xvor)*(z1-zvor))
+            az2=  0.5d+00*((x1-xvor)*(ym-yvor)-(xm-xvor)*(y1-yvor))
+
+            xarea =  ax1 + ax2
+            yarea =  ay1 + ay2
+            zarea =  az1 + az2
+
+c           print*,"area 1:   ",ax1,ay1,az1
+c           print*,"area 2:   ",ax2,ay2,az2
+c           print*,"area sum: ",xarea,yarea,zarea
+
       return
       end
