ed5964fcf0e7a87f09032390c608225bcc7560e3
Terry Miller <tamiller@lanl.gov>
2012-07-18 07:44:24 -0600

    added code for iremov option which seemed to be missing. previous calls would do nothing for iremov option. reon2 calls this option, not sure if it ever gets used.

--- src/mflip.f
+++ src/mflip.f
@@ -1,6 +1,5 @@
 *dk,mflip
-      subroutine mflip(memopt1,len,ivec)
-       implicit real*8 (a-h,o-z)
+      subroutine mflip(memopt1,len,cname)
 C
 C #####################################################################
 C
@@ -15,7 +14,7 @@ C                    0 - initialize memory
 C                    1 - increment memory
 C                    99- release memory
 C        len      - length to allocate
-C        ivec     - optional specific array
+C        cname     - optional specific array
 C
 C     OUTPUT ARGUMENTS -
 C
@@ -36,34 +35,46 @@ CPVCS    original version
 C
 C ######################################################################
 C
+      implicit none
+C
       include "neibor.h"
+
+C arguments (memopt1,len,cname)
+      integer memopt1,len
+      character*8 cname
+
+C variables
+      character*8 cnamf
+      integer ione,len4,ics
 C
 C ######################################################################
+C BEGIN begin
 C
-      character*8 ivec, cnamf
-C
-C ######################################################################
-C
+C tam - added iremov increment, create, and release july 2012
+ 
       cnamf='flipmem'
       ione=1
       len4=len*4
       if(memopt1.eq.1) then
-         if(ivec(1:6).eq.'iremov') then
-         elseif(ivec(1:5).eq.'iopen') then
+         if(cname(1:6).eq.'iremov') then
+            call mmnewlen('iremov',cnamf,ipiopen ,len ,ics)
+         elseif(cname(1:5).eq.'iopen') then
             call mmnewlen('iopen ',cnamf,ipiopen ,len ,ics)
-         elseif(ivec(1:6).eq.'ivacnt') then
+         elseif(cname(1:6).eq.'ivacnt') then
             call mmnewlen('ivacnt',cnamf,ipivacnt,len ,ics)
-         elseif(ivec(1:4).eq.'tmp2') then
+         elseif(cname(1:4).eq.'tmp2') then
             call mmnewlen('itmp2 ',cnamf,ipitmp2 ,len ,ics)
-         elseif(ivec(1:6).eq.'irclst') then
+         elseif(cname(1:6).eq.'irclst') then
             call mmnewlen('irclst',cnamf,ipirclst,len ,ics)
          endif
       elseif(memopt1.eq.99) then
+         call mmrelblk('iremov',cnamf,ipiopen ,ics)
          call mmrelblk('iopen ',cnamf,ipiopen ,ics)
          call mmrelblk('ivacnt',cnamf,ipivacnt,ics)
          call mmrelblk('itmp2 ',cnamf,ipitmp2 ,ics)
          call mmrelblk('irclst',cnamf,ipirclst,ics)
       else
+         call mmggetbk('iremov',cnamf,ipiopen ,ione,2,ics)
          call mmggetbk('iopen ',cnamf,ipiopen ,ione,2,ics)
          call mmggetbk('ivacnt',cnamf,ipivacnt,ione,2,ics)
          call mmggetbk('itmp2 ',cnamf,ipitmp2 ,ione,2,ics)
