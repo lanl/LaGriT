75d30e23b8bea02429bcbc3cfa4b53188a7eb382
Terry Miller <tamiller@lanl.gov>
2012-11-08 15:25:16 -0700

    Quan updates for refine based on attribute

--- src/massage2.f
+++ src/massage2.f
@@ -33,8 +33,9 @@ C######################################################################
  
       integer nwds, imsgin(nwds), msgtype(nwds)
       integer i, j, k, ilen, itype, ierror, ierrw, ierr, itest, iout
-      real*8 xmsgin(nwds), xmin, xmax, ipt, factor, REF_H, tmp2, test
-      integer imin, imax, ntimes, tmp1, iout1
+      real*8 xmsgin(nwds), xmin, xmax, ipt, factor, REF_H, tmp2
+      real*8 scale
+      integer imin, imax, ntimes, tmp1, iout1, nrefine
 
       character*(*) cmsgin(nwds)
       character*32 cmdsave(nwds), file_name, field_name, cmo,
@@ -47,7 +48,7 @@ C######################################################################
       pointer(ipout1, iout1)
       real*8 edge_max(10000000), field(10000000), HSCALE(10000000)
 
-      test = 1.e-2
+      nrefine = 0
 
       file_name = cmsgin(2)
       call fexist(file_name,ierr)
@@ -56,7 +57,7 @@ C######################################################################
             goto 9999
          endif
       
-      print *, msgtype(4)
+C      print *, msgtype(4)
       if(msgtype(4) .ne. 3) then
          print *, trim(cmsgin(4)),' is not a valid field'
          goto 9999
@@ -122,17 +123,6 @@ C     Create a temporary attribute called target_edge_length
      *      '/target_edge_length/' // REF_H_CHAR // ';finish'
          call dotaskx3d (cbuf, ierror)
       endif
-      cbuf = 'cmo/status/' // cmo(1:icharlnf(cmo))// ';finish'
-      call dotaskx3d (cbuf, ierror)
-      cbuf = 'cmo/printatt/' // cmo(1:icharlnf(cmo))//
-     *   '/target_edge_length;finish'
-      call dotaskx3d (cbuf, ierror)
-      call cmo_get_attinfo('target_edge_length',cmo,iout,test,cout,
-     *                        ipout1,ilen,itype, ierror)
-      print *, 'cmo name', cmo
-      print *, 'value of target edge length', test
-
-      
 C
 C     Create the edgemax array using quality
 C     and get the maximum edge length
@@ -155,22 +145,34 @@ C     Now get the maximum value
     
 C
 C     Calculate the number of times to call massage
-      ntimes = int(xmax/REF_H)
-      tmp1 = ceiling(xmax/REF_H)
-      tmp2 = float(1)/float(ntimes)
+      scale = xmax/REF_H
+      ntimes = 1
+      HSCALE(ntimes) = xmax
+      write(HSCALE_CHAR(ntimes),'(f10.4)') HSCALE(ntimes)
+      do while (scale .gt. 10)
+         scale = scale/2.0
+         HSCALE(ntimes+1) = HSCALE(ntimes)/2
+         print *, 'Values of refinement scale', HSCALE(ntimes+1)
+         write(HSCALE_CHAR(ntimes+1),'(f10.4)') HSCALE(ntimes+1)
+         ntimes = ntimes + 1
+      enddo
+
+      tmp2 = 1.0/float(int(scale))
+      tmp1 = ceiling(HSCALE(ntimes)/REF_H)
       factor = tmp1**tmp2
 
-C     Store the values for refine as both real and character
-      HSCALE(ntimes) = REF_H
-      write(HSCALE_CHAR(ntimes),'(f10.4)') HSCALE(ntimes)
-      do k = 1, ntimes-1
-         HSCALE(k) = REF_H*(factor**(ntimes-k))
+      nrefine = ntimes + int(scale)
+      do k = ntimes + 1, nrefine - 1
+         HSCALE(k) = REF_H*(factor**(nrefine-k))
          write(HSCALE_CHAR(k),'(f10.4)') HSCALE(k)
       enddo
 
+      HSCALE(nrefine) = REF_H
+      write(HSCALE_CHAR(nrefine),'(f10.4)') HSCALE(nrefine)
+
 C     Now refine iteratively      
       itest = 0
-      do i = 1, ntimes-1
+      do i = 2, nrefine-1
          itest = itest + 1
          cbuf = 'infile/' // file_name // '; finish'
          call dotaskx3d(cbuf, ierror)
@@ -199,7 +201,7 @@ C     Repeat the last refinement for good mesh quality
          cbuf = 'math/floor/' // trim(cmo) // '/'
      *      // trim(field_name) // '/1,0,0/' 
      *      // trim(cmo) // '/' // trim(field_name) // '/' 
-     *      // HSCALE_CHAR(ntimes) // ';finish'
+     *      // HSCALE_CHAR(nrefine) // ';finish'
          call dotaskx3d(cbuf, ierror)
          cbuf = 'cmo/printatt/' // trim(cmo) // '/'
      *   // trim(field_name) // '/minmax; finish'
