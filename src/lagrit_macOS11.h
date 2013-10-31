83f71b3c086ae3ae5ded75d272cea5f3648118a4
Terry Miller <tamiller@lanl.gov>
2013-10-31 10:08:47 -0600

    Updates to compile Mac OS releases 11.x and 12.x Lion and M. Lion

---
+++ src/lagrit_macOS11.h
@@ -0,0 +1,38 @@
+c
+c----------------------------------------------------------------
+c lagrit.h for mac OS 11.x Lion  arch x86_64 
+c
+c This is a template for the lagrit program banner
+c Substitute the TAG strings with Date and Linux, Darwin, SunOS, or IRIX
+c Compile library with updated lagrit.h used in writinit()
+c This template is preserved in lagrit.template.h
+c
+c----------------------------------------------------------------
+c
+      integer        v_major, v_minor
+      parameter      (v_major=3)
+      parameter      (v_minor=101)
+c
+      character*22   date_compile
+      character*8    os_name
+      character*16   my_name
+c
+      data my_name      /'lagritgen'/
+
+c     os_name is used to find and write OS related files
+c     make sure it is a version recognized in Makefile
+c     and writinit.f for forming header info
+c     data os_name      /'Darwini '/
+      data os_name      /'MacOS11 '/
+
+c     This string is used for writing LaGriT header
+c     data date_compile /'2012/07/26 gfort 4.6 '/
+      data date_compile /'2013/10/20 x64 GCC4.7'/
+c
+      integer         NCall
+      save            NCall
+      character*8     Version
+      save            Version
+c
+c----------------------------------------------------------------
+c
---
+++ src/lagrit_maci32.h
@@ -0,0 +1,32 @@
+c
+c----------------------------------------------------------------
+c lagrit.h for mac OSX intel 32 bit -arch i386 
+c
+c This is a template for the lagrit program banner
+c Substitute the TAG strings with Date and Linux, Darwin, SunOS, or IRIX
+c Compile library with updated lagrit.h used in writinit()
+c This template is preserved in lagrit.template.h
+c
+c----------------------------------------------------------------
+c
+      integer        v_major, v_minor
+      parameter      (v_major=3)
+      parameter      (v_minor=101)
+c
+      character*22   date_compile
+      character*8    os_name
+      character*16   my_name
+c
+      data my_name      /'lagritgen'/
+c     data os_name      /'Darwini '/
+c     data os_name      /'Maci64  '/
+      data os_name      /'Maci32  '/
+      data date_compile /'2013/10/20  i386 gfort'/
+c
+      integer         NCall
+      save            NCall
+      character*8     Version
+      save            Version
+c
+c----------------------------------------------------------------
+c
