c98c5b598f2f0e013a7a6e15d84ff2a8c5e53010
Dylan Robert Harp <dharp@lanl.gov>
2016-04-29 16:50:14 -0600

    Modified merge example to use modified merge method

--- examples/ex_merge_mesh_objects.py
+++ examples/ex_merge_mesh_objects.py
@@ -4,20 +4,27 @@
 
 #To use pylagrit, import the module.
 import pylagrit
+import numpy
 
 #Instantiate the lagrit object.
 lg = pylagrit.PyLaGriT()
 
-#Create mesh objects.
-mo1 = lg.create()
-mo2 = lg.create()
-mo3 = lg.create()
-mo4 = lg.create()
+# Create list with mesh object as first element
+dxyz = numpy.array([0.25]*3)
+mins = numpy.array([0.]*3)
+maxs = numpy.array([1.]*3)
+ms = [lg.createpts_dxyz(dxyz,mins,maxs,'tet',connect=True)]
 
-#Merge mesh objects into object mo_merged.
-mo_merged1 = lg.merge(mo1, mo2)
-mo_merged2 = lg.merge(mo1, mo2, mo3)
-mo_merged3 = lg.merge(mo1, mo2, mo3, mo4)
+# Create three new mesh objects, each one directly above the other
+for i in range(3):
+    ms.append(ms[-1].copy())
+    ms[-1].trans(ms[-1].mins,ms[-1].mins+numpy.array([0.,0.,1.]))
 
+# Merge list of mesh objects and clean up
+mo_merge = lg.merge(ms)
+for mo in ms: mo.delete()
+mo_merge.rmpoint_compress(filter_bool=True,resetpts_itp=True)
+
+mo_merge.paraview(filename='mo_merge.inp')
 
 
