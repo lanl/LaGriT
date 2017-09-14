0878a3c950663fcc761923c475638bc773c2b257
Nathaniel Tower Knapp <nknapp@lanl.gov>
2017-09-14 16:01:25 -0600

    removed Rakefile and other ruby stuff

--- docs/edit_scripts/flatten.py
+++ docs/edit_scripts/flatten.py
@@ -2,11 +2,11 @@ import os, sys
 
 # get names of all markdown files
 
-dr = '/home/nknapp/deknapp.github.io/pages/docs/demos/'
+dr = '/Users/nknapp/Desktop/LaGriT/docs//home/nknapp/deknapp.github.io/pages/docs/demos/'
 
-#for root, drs, fles in os.walk(dr):
- #   for fle in fles:
-  #      os.system('mv ' + os.path.join(root, fle) + ' ' + dr)
+for root, drs, fles in os.walk(dr):
+    for fle in fles:
+        os.system('mv ' + os.path.join(root, fle) + ' ' + dr)
 
 for root, drs, fles in os.walk(dr):
     for dr in drs:
