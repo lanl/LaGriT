259f35042b12211beb920339a4b5298d7252f3f1
Nathaniel Tower Knapp <nknapp@lanl.gov>
2017-09-18 13:18:07 -0600

    try to remove backlash from markdown links in site map and site index

--- docs/edit_scripts/get_site_map.py
+++ docs/edit_scripts/get_site_map.py
@@ -22,7 +22,7 @@ def print_links(root_fle, depth, max_depth, out_fle, md_file_list, already_linke
         link_list = list(set(link_list) - set(already_linked))
         for link in sorted(link_list):
             link_fle = get_file(link, md_file_list)
-            rel_link = link_fle.split('LaGriT/docs')[-1][:-3]
+            rel_link = link_fle.split('LaGriT/docs/')[-1][:-3]
             out_fle.write(indent + '[' + link[:-3] + ']' + '(' + rel_link + ')' + '\\' + '\n')
             already_linked.append(link)
             if 'release' not in link_fle:
@@ -52,7 +52,7 @@ os.system(convert)
 out_fle_name = '/Users/nknapp/Desktop/LaGrit/docs/md_site_list.md'
 out = open(out_fle_name, 'w')
 for fle in sorted(md_file_list):
-    rel_link = fle.split('LaGriT/docs')[-1][:-3]
+    rel_link = fle.split('LaGriT/docs/')[-1][:-3]
     out.write('[' + fle.split('/')[-1] + '](' + rel_link + ')' + '\\' + '\n') 
 
 convert = 'pandoc ' + out_fle_name + ' -f markdown -t html -s -o /Users/nknapp/Desktop/LaGriT/docs/site_list.html'
