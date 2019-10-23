2a0d86713cf196069e5201e27ef6ae7dff4c3b10
Daniel Reece Livingston <livingston@lanl.gov>
2019-10-23 12:33:33 -0600

    checkpoint

--- src/silo_test.f
+++ src/silo_test.f
@@ -1,53 +1,128 @@
       subroutine silo_test()
+
       implicit none
       include "silo.inc"
-c
-    
-        integer, parameter :: NNODES = 4
-        integer, parameter :: NZSHAPES = 1
-        integer, parameter :: LZNODELIST = 6
-        integer, parameter :: NZONES = 2
-        integer, parameter :: NDIMS = 2
+      integer(kind=4) builducd
+      integer(kind=4)  dbid, meshid, err, driver, nargs
+      character*256 cloption
 
-        integer silo_err, silo_ierr, optlistid
-        integer meshid
-        integer silo_dbfile
+      driver = DB_HDF5
 
-        real*8  x(NNODES), y(NNODES)
-        integer zshapesize(NZSHAPES), zshapecnt(NZSHAPES) ! 1 == NZSHAPES = 1
-        integer znodelist(LZNODELIST)
+      err = dbcreate("ucdf77.silo", 11, 0, DB_LOCAL,
+     .               "file info", 9, driver, dbid)
 
-        data zshapesize /3/ ! triangle
-        data zshapecnt  /2/ ! two triangles
+      meshid = builducd(dbid, "ucd", 3) 
+      err = dbclose(dbid)
 
-        data x /0.0,1.0,2.0,1.0/
-        data y /0.0,2.0,0.0,-2.0/
-        data znodelist /1,2,3,3,1,4/
-
-        silo_err = dbcreate("test.silo", 11, 0, DB_LOCAL,
-     &               "file info", 9, DB_HDF5, silo_dbfile)
-
-        print*,'silo_err = ',silo_err
-
-        silo_err = dbmkoptlist(3, optlistid)
-
-        silo_err = dbputzl(silo_dbfile, 'Zonelist', 8, NZONES, 2,
-     &              znodelist, LZNODELIST, 0,
-     &              zshapesize, zshapecnt, NZSHAPES, silo_ierr)
-
-        print*,'silo_err = ',silo_err
-
-
-        silo_err = dbputum(silo_dbfile, "ucd", 3, NDIMS,
-     &              x, y, DB_F77NULL,
-     &              "X", 1, "Y", 1, DB_F77NULLSTRING, 0, DB_DOUBLE,
-     &              NNODES, NZONES, 'Zonelist', 8, 
-     &              DB_F77NULLSTRING, 0,
-     &              optlistid, meshid)   
-
-        print*,'silo_err = ',silo_err     
-
-        silo_err = dbclose(silo_dbfile)
-
-c
       end subroutine silo_test
+
+ccccccccc
+
+      integer(kind=4) function builducd (dbid, name, lname)
+c----------------------------------------------------------------------
+c  Routine                            builducd
+c
+c  Purpose
+c
+c   Build ucd-mesh, ucd-var, facelist and zonelist, and return
+c   the mesh ID.
+c
+c  Modifications:
+c    Kathleen Bonnell, Wed Sep 2 16:12:15 PDT 20099
+c    Changed 'character*8 name' to 'character*(*) name' to remove 
+c    'Character length argument mismatch' compiler error.
+c
+c-----------------------------------------------------------------------
+
+      integer(kind=4)  dbid             ! Database identifier
+      character*(*) name        ! Name of mesh to build
+      integer(kind=4)     lname         ! Length of name
+
+      include "silo.inc"       ! Don't forget to include this file
+
+      integer(kind=4) :: NZONES = 7   ! Number of zones
+      integer(kind=4) :: NNODES = 13  ! Number of nodes
+      integer(kind=4) :: NZSHAPES = 1  ! Number of zone shapes
+      integer(kind=4) :: LZNODELIST = 27 ! Length of zone nodelist
+      integer(kind=4)      i, zlid, meshid, optlistid
+
+      real*4, allocatable ::        x(:), y(:)
+      integer(kind=4), allocatable ::  znodelist(:) !znodelist(LZNODELIST)
+      integer(kind=4)      zshapesize(1), zshapecnt(1)
+
+      integer lg_error, ilen, itype, n_nodes, nelems
+      character(len=90) :: cmo
+
+
+      pointer (ixic, xic)
+      pointer (iyic, yic)
+      pointer (izic, zic)
+      pointer (ipitet, elem_connectivity)
+      pointer (ipitettyp, elem_type)
+
+      real*8 xic(*), yic(*), zic(*)
+      integer*8 elem_connectivity(*), elem_type(*)
+
+      call cmo_get_name(cmo,lg_error)
+      call cmo_get_info('nnodes',cmo,n_nodes,ilen,itype,ierror)
+      call cmo_get_info('nelements',cmo,nelems,ilen,itype,ierror)
+      call cmo_get_info('xic',cmo,ixic,ilen,itype,lg_error)
+      call cmo_get_info('yic',cmo,iyic,ilen,itype,lg_error)
+      call cmo_get_info('zic',cmo,izic,ilen,itype,lg_error)
+      call cmo_get_info('itet', cmo, ipitet, ilen, itype, ierror)
+      call cmo_get_info('itettyp',cmo,ipitettyp,ilen,itype,ierror)
+
+      NNODES = long(n_nodes)
+      NZONES = nelems
+      LZNODELIST = long(nelems * elem_connectivity(1))
+
+      allocate(x(NNODES))
+      allocate(y(NNODES))
+      allocate(znodelist(LZNODELIST))
+
+
+c...Initialize coordinate and zonal data.
+      x(1:NNODES) = real(xic(1:n_nodes))
+      y(1:NNODES) = real(yic(1:n_nodes))
+
+      print*,'len = ',size(x)
+
+
+      znodelist(1:LZNODELIST) = long(elem_connectivity(1:LZNODELIST))
+
+      zshapesize(1) = long(elem_connectivity(1))
+      zshapecnt(1) = long(nelems)
+
+c...Create a rudimentary option list.
+
+      ierr = dbmkoptlist(3, optlistid)                   ! Create the option list
+
+c...Write zone list. This contains the information which describes each
+c...of the zones in the mesh. The nodelist must be organized such that
+c...zones of each shape are described consecutively.
+
+      err = dbputzl(dbid, 'Zonelist', 8, NZONES, 2, znodelist,
+     .              LZNODELIST, 0,
+     .              zshapesize, zshapecnt, NZSHAPES, zlid)
+
+
+
+c...Write the UCD mesh. For any arguments which are not applicable,
+c...use the DB_F77NULL argument. The mesh id is returned in 'meshid'.
+
+      err = dbputum(dbid, name, lname, 2, x, y, DB_F77NULL,
+     .              "X", 1, "Y", 1, DB_F77NULLSTRING, 0, DB_FLOAT,
+     .              NNODES, NZONES, 'Zonelist', 8, DB_F77NULLSTRING, 0,
+     .              optlistid, meshid)
+
+
+      deallocate(x)
+      deallocate(y)
+      deallocate(znodelist)
+
+c...Write out a UCD variable. Use dbputuv1 for scalar (non-vector)
+c...arrays. The variable ID is returned in 'varid'.
+
+
+      builducd = meshid
+      end
