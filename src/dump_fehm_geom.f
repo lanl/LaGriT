a8e5d26b1019e468b6f1175c1eb3787420fde974
Daniel Reece Livingston <livingston@lanl.gov>
2020-03-04 15:57:49 -0700

    Formatting fix for #191

--- src/dump_fehm_geom.f
+++ src/dump_fehm_geom.f
@@ -364,7 +364,7 @@ C
             i2=itet1(index+2)
             i3=itet1(index+3)
             write(iunit,9050) it,i1,i2,i3
- 9050       format(10i8)
+ 9050       format(10i12)
          enddo
       elseif(nsdtopo.eq.3.and.nen.eq.4.and.nef.eq.4) then
          ns=4
