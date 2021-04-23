a4fc5ee5179beb913fb42767d842d471f4b3d924
Daniel Reece Livingston <livingston@lanl.gov>
2021-04-23 14:27:04 -0600

    Used Black formatting on test Python files

--- test/lg_test_lib/__init__.py
+++ test/lg_test_lib/__init__.py
@@ -12,56 +12,67 @@ checkdir = None
 testfile = None
 flags = None
 
+
 class readable_dir(argparse.Action):
-    '''
+    """
     Throws an exception if a directory is non-existant.
     Otherwise, passes silently.
-    '''
-    def __call__(self,parser,namespace,values,option_string=None):
+    """
+
+    def __call__(self, parser, namespace, values, option_string=None):
         try:
             prospective_dir = values
             if not os.path.isdir(prospective_dir):
-                msg = "\nInvalid Directory: {0} is not a valid path\n".format(prospective_dir)
+                msg = "\nInvalid Directory: {0} is not a valid path\n".format(
+                    prospective_dir
+                )
                 raise argparse.ArgumentTypeError()
             if os.access(prospective_dir, os.R_OK):
-                setattr(namespace,self.dest,prospective_dir)
+                setattr(namespace, self.dest, prospective_dir)
             else:
-                msg = "\nInvalid Directory: {0} is not a readable dir\n".format(prospective_dir)
+                msg = "\nInvalid Directory: {0} is not a readable dir\n".format(
+                    prospective_dir
+                )
                 raise argparse.ArgumentTypeError()
 
         except argparse.ArgumentTypeError:
-            
             print(sys.stderr, msg)
             print("/".join(os.listdir(os.curdir)))
             sys.exit(2)
 
-def TestSingle(testcase:str,clean:bool=True,test:bool=True,check:bool=True):
-    '''
+
+def TestSingle(
+    testcase: str, clean: bool = True, test: bool = True, check: bool = True
+):
+    """
     Runs a single test case.
-    '''
-    os.chdir(os.path.join(dtop,testcase))
+    """
+    os.chdir(os.path.join(dtop, testcase))
     print(os.getcwd())
 
     if clean:
         cln.CleanSingleDir(os.getcwd())
 
-    os.chdir('..')
+    os.chdir("..")
 
     if test:
-        run.RunTest(tag=testfile,
-                    executable=lagrit_exe,
-                    flags=flags,
-                    test_dir=testcase.split('/')[-1])
+        run.RunTest(
+            tag=testfile,
+            executable=lagrit_exe,
+            flags=flags,
+            test_dir=testcase.split("/")[-1],
+        )
 
     if check:
-        chk.Check(target=checkdir,
-                  test_dir=testcase.split('/')[-1])
-    
-    os.chdir(os.path.join(dtop,testcase))
+        chk.Check(target=checkdir, test_dir=testcase.split("/")[-1])
+
+    os.chdir(os.path.join(dtop, testcase))
 
 
-def TestDir(testing_dir:str,clean:bool=True,test:bool=True,check:bool=True):
-    '''
+def TestDir(
+    testing_dir: str, clean: bool = True, test: bool = True, check: bool = True
+):
+    """
     Runs a test suite on a given directory.
 
     Args:
@@ -71,15 +82,15 @@ def TestDir(testing_dir:str,clean:bool=True,test:bool=True,check:bool=True):
        clean (bool): cleans the directory before test
        test (bool): runs a test
        check (bool): verifies test accuracy
-    '''
+    """
 
-    os.chdir(os.path.join(dtop,testing_dir))
+    os.chdir(os.path.join(dtop, testing_dir))
 
     if clean:
         cln.Clean(tag=testfile)
 
     if test:
-        run.RunTest(tag=testfile,executable=lagrit_exe,flags=flags)
+        run.RunTest(tag=testfile, executable=lagrit_exe, flags=flags)
 
     if check:
         chk.Check(target=checkdir)
