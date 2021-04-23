3ca8d468f58c52785be0f2e150597f694bb46a74
Daniel Livingston <daniel.livingston@live.com>
2021-04-23 13:39:08 -0600

    Changed PyLaGriT setup to setuptools from distutil

--- PyLaGriT/setup.py
+++ PyLaGriT/setup.py
@@ -1,17 +1,27 @@
-from distutils.core import setup
+"""
+Build script for PyLaGriT
+"""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 import os
 
-setup(name='pylagrit',
-	version='1.0.0',
-	description='Python interface for LaGriT',
-	author='Dylan R. Harp',
-	author_email='dharp@lanl.gov',
-	url='lagrit.lanl.gov',
-	license='LGPL',
-	install_requires=[
-		'pexpect==4.6.0',
-		'numpy',
-		],
-	packages=[
-		'pylagrit',]
-	)
+setup(
+    name="pylagrit",
+    version="1.0.1",
+    description="Python interface for LaGriT",
+    author="Dylan R. Harp",
+    author_email="dharp@lanl.gov",
+    url="lagrit.lanl.gov",
+    license="LGPL",
+    install_requires=[
+        "pexpect>=4.6.0",
+        "numpy",
+    ],
+    packages=[
+        "pylagrit",
+    ],
+)
