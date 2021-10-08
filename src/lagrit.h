4373c8d768684d19d77d9dfa8df19f04808da68c
Terry Miller <tamiller@lanl.gov>
2021-10-08 13:45:19 -0600

    Update lagrit.h to release V3.3.3 October 8 2021

--- src/lagrit.h
+++ src/lagrit.h
@@ -9,19 +9,19 @@ c----------------------------------------------------------------
 c
       integer        v_major, v_minor
       parameter      (v_major=3)
-      parameter      (v_minor=320)
+      parameter      (v_minor=333)
 c
       character*22   date_compile
       character*8    os_name
       character*16   my_name
 c
-      data my_name      /'lagritgen'/
+      data my_name      /'lagrit'/
 c     os_name is used to find and write OS related files
 c     make sure it is a version recognized in Makefile
 c     and writinit.f for forming header info
-      data os_name      /'Darwin'/
+      data os_name      /'Linux'/
 c
-      data date_compile /'2020/03/17 Release '/
+      data date_compile /'2021/10/08 release'/
 c
       integer         NCall
       save            NCall
--- src/lagrit.template.h
+++ src/lagrit.template.h
@@ -1,17 +1,18 @@
 c
 c----------------------------------------------------------------
-c lagrit.h for linux with 32 bit
+c lagrit.h.template 
 c
 c This is a template for the lagrit program banner
 c Substitute the TAG strings with Date and Linux, Darwin etc
 c Compile library with updated lagrit.h used in writinit()
 c This template is preserved in lagrit.template.h
+c Your own date and words can be used on the date_compile line
 c
 c----------------------------------------------------------------
 c
       integer        v_major, v_minor
       parameter      (v_major=3)
-      parameter      (v_minor=200)
+      parameter      (v_minor=333)
 c
       character*22   date_compile
       character*8    os_name
@@ -20,7 +21,7 @@ c
       data my_name      /'lagritgen'/
       data os_name      /'OSTAG   '/
       data date_compile /'DATETAG               '/
-      data date_compile /'2015/03/15 RH gf4.5   '/
+      data date_compile /'2021/10/8  release    '/
 c
       integer         NCall
       save            NCall
@@ -28,4 +29,3 @@ c
       save            Version
 c
 c----------------------------------------------------------------
-c
