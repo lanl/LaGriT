b4378726b5649d0e0fecf2c446c520364a441852
Daniel Reece Livingston <livingston@lanl.gov>
2021-12-21 15:15:43 -0700

    Release cleanup

--- examples/liblagrit/main.cxx
+++ examples/liblagrit/main.cxx
@@ -1,3 +1,8 @@
+/*
+ * Simple example illustrating linking LaGriT
+ * with an external C/C++ code.
+ */
+
 #include <iostream>
 #include <string>
 #include "lagrit.h"
@@ -7,6 +12,9 @@ int main() {
     std::string log_file(" ");
     std::string batch_file(" ");
 
+    std::string task("cmo/create/mo1///tri; finish");
+
     initlagrit(mode.c_str(), log_file.c_str(), batch_file.c_str(), mode.size(), log_file.size(), batch_file.size());
+    dotask(task.c_str(), task.size());
     return 0;
 }
