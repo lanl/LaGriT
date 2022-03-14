a11920f492b781ce68d9e8fb8f50ab5fb02fdebb
Livingston, Daniel <Daniel.Livingston@amd.com>
2022-03-14 13:33:56 -0400

    cleanup

--- cmake/LaGriT.cmake
+++ cmake/LaGriT.cmake
@@ -1,4 +1,12 @@
 function(lagrit_config_windows name)
+    add_definitions(-Dwin64)
+    set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)
+
+    if(MSVC_IDE)
+        add_compile_options(/MP)
+        set_property(GLOBAL PROPERTY USE_FOLDERS ON)
+    endif()
+
     set_target_properties(${name}
         PROPERTIES
             ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
