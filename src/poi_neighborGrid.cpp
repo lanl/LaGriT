a1d466f8b4551c0d0246d00be827dd86f3643f56
Jeffrey Hyman <you@ejhyman@lanl.gov>
2022-10-18 15:23:44 -0600

    commiting to vectors

--- src/poi_neighborGrid.cpp
+++ src/poi_neighborGrid.cpp
@@ -47,15 +47,11 @@ void Polygon::initializeNeighborGrid() {
     // Create the background neighbor grid that is numCellX by numCellsY
     // The dynamic memory allocation gets freed in the destructor of polygon.
     cout << "Initializing memory for neighbor grid" << endl;
-    // changeg this to linear indexing, because Linux was being a pita. 
-    grid = new unsigned int[numCellsX * numCellsY]();
-    int ** grid2D;
-    grid2D = new int * [numCellsX];
- 
-    for (unsigned int i = 0; i < numCellsX; i++){
-        grid2D[i] = new int[numCellsY];
+    // changeg this to linear indexing with a vector, because Linux was being a pita. 
+    grid.reserve(numCellsX * numCellsY);
+    for (unsigned int i = 0; i < numCellsX * numCellsY; i++){
+        grid.push_back(0);
     }
-    
     // for (unsigned int i = 0; i < numCellsX; i++) {
     //     // the () at the end will initialize all values to 0
     //     cout << "i: " << i << " of numCellsX: " << numCellsX << endl;
