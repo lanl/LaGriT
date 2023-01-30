049af81e9eab1aec769962bca64f323c7c540022
Jeffrey Hyman <you@ejhyman@lanl.gov>
2023-01-30 16:07:09 -0700

    remove flag

--- cmake/CompilerFlags-Fortran.cmake
+++ cmake/CompilerFlags-Fortran.cmake
@@ -1,7 +1,7 @@
 
 if ("${CMAKE_Fortran_COMPILER_ID}" MATCHES "GNU")
   message(STATUS "  Fortran compiler: GNU GFORTRAN")
-  set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -m64 -fcray-pointer -fdefault-integer-8 -std=legacy -fno-sign-zero -fno-range-check")
+  set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fcray-pointer -fdefault-integer-8 -std=legacy -fno-sign-zero -fno-range-check")
 elseif ("${CMAKE_Fortran_COMPILER_ID}" MATCHES "Intel")
   message(STATUS "  Fortran compiler: Intel Fortran")
   set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -w -O -Qm64 -Qsafe-cray-ptr -integer-size=64 -assume:nominus0 -QRimplicit-import-")
