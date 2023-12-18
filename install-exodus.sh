7109fa05f4b9af972636f2e2ea7caa753a1d4557
Terry Miller <tamiller@lanl.gov>
2023-12-18 13:36:12 -0700

    Running the script will FIX Exodus cmake files for a successful linux build

--- install-exodus.sh
+++ install-exodus.sh
@@ -3,7 +3,7 @@
 # Seacas Contact Gregory Sjaardema gsjaardema@gmail.com
 #
 # LaGriT does not need full full seacas but does require
-# exodus and associated libs:
+# exodus and associated libs with FORTRAN=YES
 # libexodus.a      libexoIIv2for32.a  libhdf5_hl.a     libnetcdf.a
 # libexodus_for.a  libhdf5.a          libhdf5_tools.a  libz.a
 # Preferable to use versions newer than July 2022
@@ -23,6 +23,37 @@ cd ${SEACAS_INSTALL_DIR}
 git clone https://github.com/sandialabs/seacas.git 
 cd seacas && export ACCESS=`pwd`
 
+# ----------------------------------------------------------------------------------------
+# Checkout a seacas tag that is known to work
+# This is no longer needed for a linux build 
+# git reset --hard v2023-11-27
+# ----------------------------------------------------------------------------------------
+# Suggestions from Greg to fix Exodus possible build errors
+#
+# Add options to TPLs/seacas/TPL/netcdf/runcmake.sh
+#
+#  cmake .. -DCMAKE_C_COMPILER:FILEPATH=${CC} \
+#           -DENABLE_PNETCDF:BOOL=${MPI} \
+#  ADD THESE OPTIONS:
+#         -DENABLE_PLUGINS:BOOL=OFF \
+#         -DENABLE_MULTIFILTERS:BOOL=NO \
+#         -DENABLE_NCZARR_FILTERS:BOOL=OFF \
+#         -DENABLE_TESTS:BOOL=OFF \
+#
+# FIX ERROR during lagrit build: routines or libs not found
+# Modify ../cmake-exodus to add curl for linux systems
+# Modify line (may leave curl off for some sytems, needed for linux)
+#
+#   EXTRA_LIB="-DSeacas_EXTRA_LINK_FLAGS=z;dl -DSEACASExodus_ENABLE_SHARED:BOOL=OFF"
+# to add curl
+#   EXTRA_LIB="-DSeacas_EXTRA_LINK_FLAGS=curl;z;dl -DSEACASExodus_ENABLE_SHARED:BOOL=OFF"
+# ----------------------------------------------------------------------------------------
+
+
+# FIX TPL/netcdf/runcmake.sh and cmake-exodus as described above
+sed -i '69i \         -DENABLE_PLUGINS:BOOL=OFF \\\n         -DENABLE_MULTIFILTERS:BOOL=NO \\\n         -DENABLE_NCZARR_FILTERS:BOOL=OFF \\\n         -DENABLE_TESTS:BOOL=OFF \\' TPL/netcdf/runcmake.sh
+sed -ie 's/DSeacas_EXTRA_LINK_FLAGS=z;dl/DSeacas_EXTRA_LINK_FLAGS=curl;z;dl/g' cmake-exodus
+
 # Special handling for Cygwin
 if [[ `uname -s` == *"CYGWIN"* ]]; then
 	BUILD=NO ./install-tpl.sh;
@@ -35,52 +66,14 @@ fi;
 # These flags are recommended for LaGriT
 CGNS=NO MATIO=NO GNU_PARALLEL=NO FMT=NO SHARED=NO NEEDS_ZLIB=YES ./install-tpl.sh 
 
-# IF ERROR: couldn't build NetCDF. exiting 
-#
-# EDIT TPLs/seacas/TPL/netcdf/runcmake.sh to add options: 
-#
-#  cmake .. -DCMAKE_C_COMPILER:FILEPATH=${CC} \
-#           -DBUILD_SHARED_LIBS:BOOL=${SHARED} \
-#           -DBUILD_TESTING:BOOL=OFF \
-#           -DCMAKE_INSTALL_PREFIX=${INSTALL_PATH} \
-#           -DCMAKE_INSTALL_LIBDIR:PATH=lib \
-#           -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
-#           -DENABLE_PNETCDF:BOOL=${MPI} \
-#  ADD THESE LINES (without the pound character):
-#         -DENABLE_PLUGINS:BOOL=OFF \
-#         -DENABLE_MULTIFILTERS:BOOL=NO \
-#         -DENABLE_NCZARR_FILTERS:BOOL=OFF \
-#         -DENABLE_TESTS:BOOL=OFF \
-#
-# run again:
-#   cd TPLs/seacas && export ACCESS=`pwd`
-#   CGNS=NO MATIO=NO GNU_PARALLEL=NO FMT=NO SHARED=NO NEEDS_ZLIB=YES ./install-tpl.sh
-#
 # Result: lib/libhdf5.a lib/libhdf5_hl.a lib/libhdf5_tools.a lib/libnetcdf.a
 #
 # ==== RUN CMAKE  ====================================
-# You can edit the cmake-exodus file to adjust compilers and settings
-# FORTRAN must be set to YES
 
 cd $ACCESS
 mkdir build && cd build
 FORTRAN=YES SHARED=NO ../cmake-exodus
 
-# ERROR during lagrit build: routines or libs not found
-# Best to edit this line before building lagrit when error will occur
-#
-# EDIT ../cmake-exodus
-#
-# Modify line (may leave curl off for some sytems, needed for linux)
-#   EXTRA_LIB="-DSeacas_EXTRA_LINK_FLAGS=z;dl -DSEACASExodus_ENABLE_SHARED:BOOL=OFF"
-# to add curl
-#   EXTRA_LIB="-DSeacas_EXTRA_LINK_FLAGS=curl;z;dl -DSEACASExodus_ENABLE_SHARED:BOOL=OFF"
-#
-# REMOVE broken cmake files: from seacas: rm -fr build
-# rerun:
-# mkdir build && cd build
-# FORTRAN=YES SHARED=NO ../cmake-exodus
-#
 # result:
 # -- Build files have been written to: TPLs/seacas/build
 #    HAVE_NETCDF: YES
@@ -100,7 +93,7 @@ make && make install
 # include/exodus_config.h  include/exodusII.h  include/exodusII.inc  include/exodusII_par.h
 # include/netcdf.h include/hdf5.h
 # 
-# Once Exodus is installed, use the following commands to build from LaGriT top:
+# Once Exodus is installed, use the following commands to build lagrit executable:
 # 
 # mkdir build/ && cd build/
 # cmake .. -DLAGRIT_BUILD_EXODUS=ON
